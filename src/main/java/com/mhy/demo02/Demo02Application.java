28dd8c3a985a4b04e10ddf18692a5efd28a5ae48
huanyu.meng <1056721680@qq.com>
2024-04-30 12:03:38 +0800

    1.0.0 demo

---
+++ src/main/java/com/mhy/demo02/Demo02Application.java
@@ -0,0 +1,14 @@
+package com.mhy.demo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo02Application.class, args);
+        System.out.println("启动成功");
+    }
+
+}
